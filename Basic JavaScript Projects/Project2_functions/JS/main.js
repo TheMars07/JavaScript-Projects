f341fd5344ebb5d6ece4ea0dc950a61ba288d491
TheMars07 <miguelsobral3607@gmail.com>
2023-08-18 11:55:30 +0100

    Project2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//Function that uses the += operator
+function myFunction(){
+    var A = "This sentence uses";
+    A += " the += operator!";
+    document.getElementById("operator").innerHTML = A;
+}
