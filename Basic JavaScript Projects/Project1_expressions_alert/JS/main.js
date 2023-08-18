e8f92aa10351b27fa263bc88ac06a25e1c40d55a
TheMars07 <miguelsobral3607@gmail.com>
2023-08-18 11:07:55 +0100

    Project1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+//window.alert() Method
+window.alert("Hello World!");
+
+//document.write() Method
+document.write('Hello, world!<br>');
+
+//Assign a variable
+var A = "This is a string.<br>";
+document.write(A);
+
+//Assign a second variable
+var B = "Second string.<br>";
+document.write(B);
+
+//String that contains an apostrophe and/or quotations marks
+document.write("Amy told Peter, \"Stop it! Or you\'ll be grounded!\"<br>");
+
+//Concatenate a string
+document.write("Today the weather" + " is perfect!<br>");
+
+//Assign a concatenated string value to a variable
+var C = "This is how you" + " concatenate two strings.<br>";
+document.write(C);
+
+//Assign multiple variables in one statement
+var car1 = "BMW", car2 = "Audi", car3 = "Vauxhall", car4 = "Volkswagen<br>", car5 = "Peugeot";
+document.write(car4);
+
+//Write an expression
+document.write(2*3);
